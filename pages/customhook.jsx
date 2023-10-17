49a756893e7a8bc08b8e6b38b0b00966988750c1
Erick Fernandes <erick.gomes.fernandes.egf@gmail.com>
2023-10-17 00:40:00 -0300

    Criado exemplo de hook para fetch

---
+++ pages/customhook.jsx
@@ -0,0 +1,17 @@
+import useFetch from "../src/hooks/useFetch"
+
+export default function CustomHookPage() {
+  const [data, loading] = useFetch("https://jsonplaceholder.typicode.com/users")
+
+  console.log(data)
+  return (
+    <div>
+      <h4>Exemplo hook customizado:</h4>
+      {loading && <p>Carregando...</p>}
+      { data?.map(({id, name}) => (
+          <div key={`item-${id}`}>{name}</div>
+        ))
+      }
+    </div>
+  )
+}
